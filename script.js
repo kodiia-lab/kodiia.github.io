7cbd73b35f8f6f91a9e647be59458959d24d70b6
KODIIA <48256619+KODIIA@users.noreply.github.com>
2020-07-22 22:43:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
 console.log("Добро пожаловать на страницу Кодии! Welcome to Kodiia page!");
 
 var firebaseConfig = {
@@ -27,54 +24,53 @@ let userMessage = document.querySelector("#userMessage");
 const db = firestore.collection("contactData");
 
 
-function formSend(){
+function formSend() {
   let userNameTestInput = userName.value;
   let userPhoneTestInput = userPhone.value;
   let userMailTestInput = userMail.value;
   let userMessageTestInput = userMessage.value;
-  
+
   db.doc(userMailTestInput)
     .set({
-    name: userNameTestInput,
-    phone: userPhoneTestInput,
-    mail: userMailTestInput,
-    message: userMessageTestInput
-  })
-};
+      name: userNameTestInput,
+      phone: userPhoneTestInput,
+      mail: userMailTestInput,
+      message: userMessageTestInput
+    });
+}
 
-function formTest(){
+function formTest() {
   let userNameTestInput = userName.value;
   let userPhoneTestInput = userPhone.value;
   let userMailTestInput = userMail.value;
   let userMessageTestInput = userMessage.value;
-  
+
   db.doc("test")
     .set({
-      text: "OK"
+    text: "OK"
   });
-
+}
 
 formTest();
 
-let hcR = Math.floor(Math.random()*50+150);
-let hcG = Math.floor(Math.random()*50+150);
-let hcB = Math.floor(Math.random()*50+200);
-let acR = Math.floor(Math.random()*50+200);
-let acG = Math.floor(Math.random()*50+150);
-let acB = Math.floor(Math.random()*50+200);
-let ccR = Math.floor(Math.random()*50+150);
-let ccG = Math.floor(Math.random()*50+200);
-let ccB = Math.floor(Math.random()*50+200);
-let scR = Math.floor(Math.random()*50+200);
-let scG = Math.floor(Math.random()*50+200);
-let scB = Math.floor(Math.random()*50+150);
-let cfcR = Math.floor(Math.random()*50+200);
-let cfcG = Math.floor(Math.random()*50+200);
-let cfcB = Math.floor(Math.random()*50+200);
-let fcR = Math.floor(Math.random()*50+200);
-let fcG = Math.floor(Math.random()*50+200);
-let fcB = Math.floor(Math.random()*50+200);
-
+let hcR = Math.floor(Math.random() * 50 + 100);
+let hcG = Math.floor(Math.random() * 50 + 100);
+let hcB = Math.floor(Math.random() * 50 + 200);
+let acR = Math.floor(Math.random() * 50 + 200);
+let acG = Math.floor(Math.random() * 50 + 100);
+let acB = Math.floor(Math.random() * 50 + 200);
+let ccR = Math.floor(Math.random() * 50 + 100);
+let ccG = Math.floor(Math.random() * 50 + 200);
+let ccB = Math.floor(Math.random() * 50 + 200);
+let scR = Math.floor(Math.random() * 50 + 200);
+let scG = Math.floor(Math.random() * 50 + 200);
+let scB = Math.floor(Math.random() * 50 + 100);
+let cfcR = Math.floor(Math.random() * 50 + 200);
+let cfcG = Math.floor(Math.random() * 50 + 200);
+let cfcB = Math.floor(Math.random() * 50 + 200);
+let fcR = Math.floor(Math.random() * 50 + 200);
+let fcG = Math.floor(Math.random() * 50 + 200);
+let fcB = Math.floor(Math.random() * 50 + 200);
 
 let sA = 0;
 let showAbout = () => {
@@ -128,10 +124,15 @@ let showContact = () => {
 }
   };
 
-
-document.getElementById("header").style.backgroundColor = "rgb(" + hcR +"," + hcG + "," +  hcB + ")";
-document.getElementById("about").style.backgroundColor = "rgb(" + acR +"," + acG + "," +  acB + ")";
-document.getElementById("course").style.backgroundColor = "rgb(" + ccR +"," + ccG + "," +  ccB + ")";
-document.getElementById("study").style.backgroundColor = "rgb(" + scR +"," + scG + "," +  scB + ")";
-document.getElementById("contact").style.backgroundColor = "rgb(" + cfcR +"," + cfcG + "," +  cfcB + ")";
-document.getElementById("footer").style.backgroundColor = "rgb(" + fcR +"," + fcG + "," +  fcB + ")";
+document.getElementById("header").style.backgroundColor =
+  "rgb(" + hcR + "," + hcG + "," + hcB + ")";
+document.getElementById("about").style.backgroundColor =
+  "rgb(" + acR + "," + acG + "," + acB + ")";
+document.getElementById("course").style.backgroundColor =
+  "rgb(" + ccR + "," + ccG + "," + ccB + ")";
+document.getElementById("study").style.backgroundColor =
+  "rgb(" + scR + "," + scG + "," + scB + ")";
+document.getElementById("contact").style.backgroundColor =
+  "rgb(" + cfcR + "," + cfcG + "," + cfcB + ")";
+document.getElementById("footer").style.backgroundColor =
+  "rgb(" + fcR + "," + fcG + "," + fcB + ")";
