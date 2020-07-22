5f7d8904302313b35a46138a6f9c874820e84c75
KODIIA <48256619+KODIIA@users.noreply.github.com>
2020-07-22 16:37:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -137,9 +137,9 @@ let showContact = () => {
     sCont=0;
   }
   if (sCont == 1){
-  document.getElementById("contactHead").style.display = "block";
+  document.getElementById("contactText").style.display = "block";
 } else if (sCont == 0) {
-  document.getElementById("contactHead").style.display = "none";
+  document.getElementById("contactText").style.display = "none";
 }
   };
 
